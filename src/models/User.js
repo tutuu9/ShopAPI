972423139d3942bad2c29bb40fec30beeb10dc07
TUTUU9 <tarasuzun@gmail.com>
2026-04-07 18:47:08 +0200

    feat: add password hashing and comparePassword method to User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -27,6 +28,20 @@ const userSchema = new mongoose.Schema({
     },
 
 }, { timestamps: true });
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+userSchema.methods.comparePassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
