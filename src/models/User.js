1fe2ebacc4f51d2ce1088a52e3d657b9c1c574cf
TUTUU9 <tarasuzun@gmail.com>
2026-04-07 18:34:15 +0200

    feat: create User model with schema validation and timestamps

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+
+}, { timestamps: true });
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
