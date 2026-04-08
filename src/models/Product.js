899b6a705fe25a46e2fd02fb55caadd39ee338df
TUTUU9 <tarasuzun@gmail.com>
2026-04-08 20:35:21 +0200

    feat: create Product model

---
+++ src/models/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    stock:{
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true } );
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
